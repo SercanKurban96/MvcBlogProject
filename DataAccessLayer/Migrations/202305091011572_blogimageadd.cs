a5181c708a2e9e607cebed41fa7e519ce707832a
Sercan KURBAN <sercankurban1996@gmail.com>
2023-06-05 13:33:54 +0300

    Add project files.

---
+++ DataAccessLayer/Migrations/202305091011572_blogimageadd.cs
@@ -0,0 +1,20 @@
+﻿namespace DataAccessLayer.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class blogimageadd : DbMigration
+    {
+        public override void Up()
+        {
+            AddColumn("dbo.Blogs", "BlogImage2", c => c.String(maxLength: 250));
+            AddColumn("dbo.Blogs", "BlogImage3", c => c.String(maxLength: 250));
+        }
+        
+        public override void Down()
+        {
+            DropColumn("dbo.Blogs", "BlogImage3");
+            DropColumn("dbo.Blogs", "BlogImage2");
+        }
+    }
+}
---
+++ DataAccessLayer/Migrations/202305191724401_mig10.cs
@@ -0,0 +1,20 @@
+﻿namespace DataAccessLayer.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class mig10 : DbMigration
+    {
+        public override void Up()
+        {
+            AddColumn("dbo.Comments", "BlogRating", c => c.Int(nullable: false));
+            DropColumn("dbo.Comments", "UserPhoto");
+        }
+        
+        public override void Down()
+        {
+            AddColumn("dbo.Comments", "UserPhoto", c => c.String(maxLength: 500));
+            DropColumn("dbo.Comments", "BlogRating");
+        }
+    }
+}
